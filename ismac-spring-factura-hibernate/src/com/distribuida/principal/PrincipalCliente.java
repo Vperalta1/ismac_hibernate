0822d2fafd65d411ec67ff25096517f37bb29458
NOCTURNO <NOCTURNO@DESKTOP-0DGVQ22>
2024-01-03 20:08:40 -0500

    03/01/2024

---
+++ ismac-spring-factura-hibernate/src/com/distribuida/principal/PrincipalCliente.java
@@ -0,0 +1,30 @@
+package com.distribuida.principal;
+
+import java.util.List;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.distribuida.dao.ClienteDAO;
+import com.distribuida.entities.Cliente;
+
+public class PrincipalCliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassPathXmlApplicationContext context =new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		   
+		ClienteDAO clienteDAO =context.getBean("clienteDAOImpl",ClienteDAO.class);
+		
+		//CRUD
+		//findAll
+		List<Cliente> clientes=clienteDAO.findALL();
+		
+		//impresion
+		for (Cliente item : clientes) {
+			System.out.println(item.toString());
+		}
+		
+		context.close();
+	}
+
+}
